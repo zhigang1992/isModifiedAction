2ea663a27f3d741015a303835658362753b10fdc
Kyle Fang <zhigang1992@gmail.com>
2019-01-17 11:18:19 +0800

    Parse event.json

--- isModified.js
+++ isModified.js
@@ -6,9 +6,9 @@ const inputs = process.argv[2].split(' ')
 
 console.log(`input is: ${inputs}`)
 
-const events = fs.readFileSync(process.env.GITHUB_EVENT_PATH, {
+const events = JSON.parse(fs.readFileSync(process.env.GITHUB_EVENT_PATH, {
     encoding: 'utf-8'
-})
+}))
 
 for (const commit of events.commits) {
     const modifications = commit.added
