7f3327270dceb65a160e3aa81344a952ca0c7b59
Kyle Fang <zhigang1992@gmail.com>
2019-01-17 11:15:04 +0800

    Add file check

--- isModified.js
+++ isModified.js
@@ -2,10 +2,26 @@ const process = require('process');
 const fs = require('fs');
 
 
-console.log(process.argv[0])
+const inputs = process.argv[3].split(' ')
+
+console.log(`input is: ${inputs}`)
 
 const events = fs.readFileSync(process.env.GITHUB_EVENT_PATH, {
     encoding: 'utf-8'
 })
 
-console.log(events)
+for (const commit of events.commits) {
+    const modifications = commit.added
+        .concat(commit.removed)
+        .concat(commit.modified)
+    for (const mod of modifications) {
+        for (const input of inputs) {
+            if (mod.startsWith(input)) {
+                console.log(`Found modification: ${mod}`)
+                process.exit(0);
+            }
+        }
+    }
+}
+
+process.exit(78)
